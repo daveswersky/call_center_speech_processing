3eba15256b414848ccc0efb67a4b4bb92e0fda53
dswersky <dswersky@gmail.com>
2022-06-05 15:17:30 -0500

    Initial Commit

---
+++ function.go
@@ -0,0 +1,17 @@
+package function
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloWorld", helloWorld)
+}
+
+// helloWorld writes "Hello, World!" to the HTTP response.
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
