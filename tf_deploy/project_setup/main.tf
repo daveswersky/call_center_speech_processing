31c89091fe204314b01d5f77d99d8997ab34365c
dswersky <dswersky@gmail.com>
2022-06-16 09:30:47 -0500

    added Cloud Run invoker to service account permissions

--- tf_deploy/project_setup/main.tf
+++ tf_deploy/project_setup/main.tf
@@ -58,6 +58,12 @@ resource "google_project_iam_member" "functions-binding" {
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
+resource "google_project_iam_member" "run-binding" {
+  project = var.project_id
+  role = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
 resource "google_project_iam_member" "bigquery-binding" {
   project = var.project_id
   role    = "roles/bigquery.dataEditor"
